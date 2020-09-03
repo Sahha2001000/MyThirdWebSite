2327cae1d30205138aa128912034a4f374a7e299
Sahha2001000 <46458055+Sahha2001000@users.noreply.github.com>
2020-09-03 16:17:39 +0300

    Version 0.1.0 website doesn't have responsive design

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.header__slider-items').slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  arrows: false,
+  dotsClass: "header__slick-dots"
+}); 
